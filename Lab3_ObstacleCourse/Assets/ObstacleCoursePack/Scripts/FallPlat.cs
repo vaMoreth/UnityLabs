bcef9fe5d34f33e2efb3314a21ac4e7b99225184
vaMoreth <jummyvasya24@gmail.com>
2024-03-24 11:13:21 +0100

    add lab3

---
+++ Lab3_ObstacleCourse/Assets/ObstacleCoursePack/Scripts/FallPlat.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallPlat : MonoBehaviour
+{
+    public float fallDelay = 0.5f;
+    public float returnDelay = 2f;
+    private Vector3 startPosition;
+    private Renderer renderer;
+    private Collider collider;
+
+    void Start()
+    {
+        startPosition = transform.position; 
+        renderer = GetComponent<Renderer>();
+        collider = GetComponent<Collider>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+	{
+		foreach (ContactPoint contact in collision.contacts)
+		{
+			if (collision.gameObject.tag == "Player")
+			{
+				StartCoroutine(FallAndReturn());
+			}
+		}
+	}
+
+    IEnumerator FallAndReturn()
+    {
+        yield return new WaitForSeconds(fallDelay);
+
+        // Замість вимкнення об'єкта, робимо його невидимим і вимикаємо коллайдер
+        renderer.enabled = false;
+        collider.enabled = false;
+
+        yield return new WaitForSeconds(returnDelay);
+
+        // Відновлюємо позицію, робимо об'єкт видимим і вмикаємо коллайдер
+        transform.position = startPosition;
+        renderer.enabled = true;
+        collider.enabled = true;
+    }
+}
